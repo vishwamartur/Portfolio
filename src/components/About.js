57df25bb590a51e75116c8b09004d2a74459091a
Vishwanath Martur <64204611+vishwamartur@users.noreply.github.com>
2024-02-21 14:28:49 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,10 +11,10 @@ export default function About() {
         <div className="col-lg-6 col-md-12">
           <h2 className="text-primary">About Me</h2>
           <p className="lead">
-            Hi, Vishwanath M, a Product developer based in Bengaluru, India. I love creating
+            Hi, <b>Vishwanath M,</b>a Product Engineer based in Bengaluru, India. I love creating
             beautiful and functional websites using modern technologies like React, Node, MongoDB,
             etc. I have over 2 years of experience in web development and have worked on various
-            projects ranging from e-commerce, social media, education, and more.
+            projects ranging from e-commerce, social media, infrastructure used websites, and more.
           </p>
           <p className="lead">
             I'm passionate about learning new skills and exploring new challenges. I'm always open
